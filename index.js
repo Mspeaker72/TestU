bcb9edaf583d1e1e3fd3679bc13e959896a9c985
Lungelo Hlatshwayo <141138516+Mspeaker72@users.noreply.github.com>
2023-11-16 08:17:42 +0200

    Update index.js
    -Lambda now only runs csv file from s3 bucket

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
-const { readFile } = require('fs/promises');
 const { parse } = require('csv-parse');
 const AWS = require('aws-sdk');
-const fs = require('fs');
-const csvParse = require('csv-parse');
 
 exports.handler = async (event) => {
-    function isCorrect(answer) {
-        return true;
-    }
-
-    let score = 0;
-    const csvFilePath = `${__dirname}/Test_questions.csv`;
-    const answerFilePath = 'Test_answer.csv';
-    let dynamodb = new AWS.DynamoDB.DocumentClient();
-    const stream = fs.createReadStream(csvFilePath);
-    const parser = csvParse.parse({ delimiter: ',', from_line: 2 });
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const s3 = new AWS.S3();
+    const parser = parse({ delimiter: ',', from_line: 2 });
 
     let questions = {}; // Use an object to store questions and answers
 
-    if (event.message == "start") {
-        return new Promise((resolve, reject) => {
-            stream.on('error', function (error) {
-                reject({ "error": "Error reading CSV file", "details": error.message });
-            }).pipe(parser)
-                .on('data', (row) => {
-                
-                    const question = row[0];
-                    const answers = row.slice(1);
-                    //array is slice from index one ommiting the question , shift() changes the original array by removing index one.
-                    questions[question] = answers;
-                })
-                .on('end', async function () {
-                    try {
-                        const promises = [];
+    try {
+        if (event.message === "start") {
+            const S3params = {
+                Bucket: "testsystemstorage",
+                Key: "Test_Template.csv",
+            };
 
-                        for (let question in questions) {
+            const data = await s3.getObject(S3params).promise();
+            const csvData = data.Body.toString('utf-8');
+
+            parser.write(csvData);
+            parser.end();
+
+            return new Promise((resolve, reject) => {
+                parser.on('readable', function () {
+                    let row;
+                    while ((row = parser.read())) {
+                        const question = row[0].trim(); // Trim leading and trailing whitespaces
+                        const answers = row.slice(1).map(answer => answer.trim()); // Trim each answer
+                        questions[question] = answers;
+                    }
+                });
+
+                parser.on('end', async function () {
+                    try {
+                        const promises = Object.entries(questions).map(([question, answers]) => {
                             let params = {
                                 TableName: 'TestQuestions',
                                 Item: {
-                                    question: question,
-                                    answers: questions[question]
+                                    question,
+                                    answers,
                                 },
                             };
-                            promises.push(dynamodb.put(params).promise());
-                        }
+                            return dynamodb.put(params).promise();
+                        });
 
                         await Promise.all(promises);
                         resolve({ "message": "You have populated the DB" });
@@ -51,33 +50,13 @@ exports.handler = async (event) => {
                         reject({ "error": "Error adding questions to DynamoDB", "details": error.message });
                     }
                 });
-        });
-    }
-//future versions of this code will not contain the test functionality , workflows will be serperated.
-
-    if (event.message == "get_question") {
-        const reply = {
-            status: "pulling from DynamoDB",
-            question: "current_question",
-            answer: "list of answers",
-            statusCode: 200
-        };
-        return reply;
-    }
-
-    if (event.answer != null) {
-        if (isCorrect(event.answer)) {
-            score += 1;
-            const reply = {
-                status: "evaluating",
-                score: score,
-                statusCode: 200
-            };
-            return reply;
+            });
         }
+
+        // Handle other cases or return a default response
+        return { "message": "Not Found", "statusCode": 404 };
+    } catch (error) {
+        // Handle general errors
+        return { "error": "An error occurred", "details": error.message };
     }
-
-
-    return { "message": "Not Found", "statusCode": 404 };
 };
-
