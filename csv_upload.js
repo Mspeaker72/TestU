c2a0b271460695595baf0b3cb45ee4a33589ec79
Lungelo Hlatshwayo <141138516+Mspeaker72@users.noreply.github.com>
2023-11-17 13:54:34 +0200

    Update csv_upload.js
    - issue with uploading an entire csv
    - can upload each question then the aws lambda function will create a new csv file 
    -using this process to reverse engineer a solution for an entire file without the use of libraries.

--- csv_upload.js
+++ csv_upload.js
@@ -1,58 +1,43 @@
-cconst AWS = require('aws-sdk');
-const fs = require('fs');
+const AWS = require('aws-sdk');
 
-/*Common use for the File System module:
--Read files
--Create files
--Update files
--Delete files
--Rename files
-*/
-AWS.config.update({region:'eu-north-1'});
+// Create an S3 instance
 const s3 = new AWS.S3();
 
-function upload_csv_to_s3(filePath,s3bucketname,Key_name){
-  
-  const fileContent = fs.readFileSync(filePath);
-  
-  const params = {
-        Bucket: s3bucketname,
-        Key: Key_name,
-        Body: fileContent,
-    };
-    
-    return s3.upload(params).promise();
-
-}
-
-
 exports.handler = async (event) => {
-  
-  if(event.message =='upload_csv'){
-    
-    const success = {
-    statusCode: 200,
-    body:"file has been uploaded to database",
-    };
-    
-     const failure = {
-    statusCode: 500,
-    body:"an error has occured during the upload process , please check format",
-    };
-    
-    upload_csv_to_s3(event.filepath,event.s3bucketname,event.templateName)
-    .then(success=> {
-      return success;
-    })
-    .catch(failure=>{
-      return failure
-    });
-    
-  }
-  
-  const defaultResponse = {
-    statusCode: 404,
-    body: "Nothing was found , invalid request"
-  };
-  return defaultResponse;
+    try {
+       
+       
+        // Create a CSV string
+        const csvData = `"Question","Answer"\n"${event.question}","${event.answer}"\n`;
+
+        // Define the S3 bucket and file name
+        const bucketName = 'testsystemstorage';
+        const fileName = 'output.csv';
+
+        // Upload the CSV data to S3
+        await s3.putObject({
+            Bucket: bucketName,
+            Key: fileName,
+            Body: csvData,
+        }).promise();
+
+        // Return a response indicating success
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'CSV file uploaded to S3 successfully' }),
+        };
+
+        return response;
+    } catch (error) {
+        // Handle errors
+        console.error('Error:', error);
+
+        // Return an error response
+        const response = {
+            statusCode: 500,
+            body: JSON.stringify({ message: error}),
+        };
+
+        return response;
+    }
 };
